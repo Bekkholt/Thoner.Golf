5afb4b805291cf55ffaf3be7566d88827ed59410
Tina <tina.bekkholt@hotmail.com>
2024-07-02 12:10:10 +0200

    Default background

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <header className="App-header">
+      <h1 className="text-3xl	text-center text-slate-50	">
+        Her kommer snart Thoner.Golf...
+      </h1>
+    </header>
+  );
+}
+
+export default App;
+
