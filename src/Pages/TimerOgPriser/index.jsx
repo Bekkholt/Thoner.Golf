e01e23a550a1160f01e6e25531d44eca339a43cd
Tina <tina.bekkholt@hotmail.com>
2024-08-06 16:08:09 +0200

    timer og priser text

--- src/Pages/TimerOgPriser/index.jsx
+++ src/Pages/TimerOgPriser/index.jsx
@@ -9,20 +9,35 @@ export default function TimerOgPriser() {
           Timer og priser
         </h1>
         <div>
-          <h2 className="text-color karla-text-bold pl-5">Type time</h2>
+          <h2 className="text-color karla-text-bold pl-5">
+            Leksjon 25 minutter: 300 kr
+          </h2>
           <p className="text-color karla-text text-l w-50 pb-5 px-5">
-            Et par avsnitt her om hvem Heidi er og hva Heidi gjør og kan tilby
-            av ditt og datt.
+            Anbefales som oppfølgingstime eller om man har et konkret område man
+            ønsker å jobbe med.
           </p>
-          <h2 className="text-color karla-text-bold pl-5">Type time</h2>
+          <h2 className="text-color karla-text-bold pl-5">
+            Leksjon 50 minutter: 550 kr
+          </h2>
           <p className="text-color karla-text text-l w-50 pb-5 px-5">
-            Et par avsnitt her om hvem Heidi er og hva Heidi gjør og kan tilby
-            av ditt og datt.
+            Anbefales ved førstegangsleksjon, og om man har et mer sammensatt
+            fokusområde man ønsker å jobbe med. Kan også benyttes om man ønsker
+            mer tid enn ved en 25 minutters leksjon.
           </p>
-          <h2 className="text-color karla-text-bold pl-5">Type time</h2>
+          <h2 className="text-color karla-text-bold pl-5">
+            Leksjon på bane, 9 hull: 1300 kr
+          </h2>
+          <p className="text-color karla-text text-l w-50 pb-5 px-5">
+            Dette er en leksjon der jeg blir med deg ut på banen, og bidra med
+            rådgivning underveis, eller om man ønsker å fokusere på noe helt
+            konkret.
+          </p>
+          <h2 className="text-color karla-text-bold pl-5">
+            Treningsgruppe for damer: 100 kr per trening, eller klippekort 10
+            treninger for 800 kr
+          </h2>
           <p className="text-color karla-text text-l w-50 px-5">
-            Et par avsnitt her om hvem Heidi er og hva Heidi gjør og kan tilby
-            av ditt og datt.
+            Påmelding skjer gjennom gruppen i SPOND-appen: Kode LHPPC
           </p>
         </div>
       </S.TextContent>
